02cac7a997bdb5d79808940bdfddb8385f348b7f
jkuehner <jochen.kuehner@kardex.com>
2025-06-23 21:44:07 +0200

    sample app for using tia file format

--- src/SampleApp/SampleApp/Program.cs
+++ src/SampleApp/SampleApp/Program.cs
@@ -1,7 +1,25 @@
 ﻿using TiaFileFormat;
 using TiaFileFormat.Database;
+using TiaFileFormat.ExtensionMethods;
+
+var networkInformationConverter = new TiaFileFormat.Wrappers.Controller.Network.NetworkInformationConverter();
 
 var tfp = TiaFileProvider.CreateFromSingleFile("V19.plf"); //Could be a Tia-Project File, a archived Project or a PLF
 var database = TiaDatabaseFile.Load(tfp);
 
-var root = database.RootObject;
+var cpus = database.ProjectRoot.Traverse(x => x.ProjectTreeChildren).Where(x => x.StorageBusinessObjectType == TiaFileFormat.Database.StorageTypes.StorageBusinessObjectType.Plc);
+foreach (var cpu in cpus)
+{
+    var networkInfos = cpu.Traverse(x => x.ProjectTreeChildren)
+        .Where(x => networkInformationConverter.GetHighLevelObjectType(x) == TiaFileFormat.Wrappers.HighLevelObjectType.NetworkInformation)
+        .Select(x => networkInformationConverter.Convert(x, null))
+        .OfType<TiaFileFormat.Wrappers.Controller.Network.NetworkInformation>()
+        .ToList();
+
+    Console.WriteLine("CPU: " + cpu.Name + " (" + cpu.Path + ")");
+    foreach(var nw in networkInfos)
+    {
+        Console.WriteLine("  " + nw.Name + ": " + nw.IpAddress);
+    }
+}
+Console.ReadLine();
