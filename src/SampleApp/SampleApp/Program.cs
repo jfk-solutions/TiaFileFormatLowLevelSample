797f659a3052dda08e211e467fa6e93259072a48
jkuehner <jochen.kuehner@kardex.com>
2025-06-23 21:05:41 +0200

    initial checkin

---
+++ src/SampleApp/SampleApp/Program.cs
@@ -0,0 +1,7 @@
+﻿using TiaFileFormat;
+using TiaFileFormat.Database;
+
+var tfp = TiaFileProvider.CreateFromSingleFile("V19.plf"); //Could be a Tia-Project File, a archived Project or a PLF
+var database = TiaDatabaseFile.Load(tfp);
+
+var root = database.RootObject;
